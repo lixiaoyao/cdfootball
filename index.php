9b30ddd9fbf117c010f6f849df92749c2f4f2cd1
xiaoyao <476552238@qq.com>
2012-07-15 11:35:04 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+<title>Login</title>
+<link href="" rel="stylesheet" type="css/text">
+</head>
+
+<body>
+  <table>
+    <tr class="first">
+      <td class="firsttd"></td>
+      <td class="firsttd"></td>
+      <td class="firsttd"></td>
+    </tr>
+    <tr class="second">
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr class="last">
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </table>
+</body>
+</html>
